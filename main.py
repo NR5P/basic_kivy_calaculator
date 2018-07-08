6c2521714d40bc366d4301a8afa48342d2fcd9d3
Nathan Rigg <orangev8zcar@gmail.com>
2018-07-08 09:10:51 -0500

    added more functionality

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import kivy
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 
+
 class CalcGridLayout(GridLayout):
     def calculate(self, calculation):
         if calculation:
@@ -11,9 +12,27 @@ class CalcGridLayout(GridLayout):
             except Exception:
                 self.display.text = "Error"
 
+    def operator_check(self, operator):
+        operator_list = ["/", "*", "-", "+"]
+        if self.display.text[-1] in operator_list:
+            self.display.text = self.display.text[:-1]
+            self.display.text += operator
+        else:
+            self.display.text += operator
+
+    def period_check(self):
+        if self.display.text[-1] != ".":
+            self.display.text += "."
+
+    def del_function(self):
+        self.display.text = self.display.text[:-1]
+
 
 class CalculatorApp(App):
+    title = "basic kivy calculator"
+
     def build(self):
         return CalcGridLayout()
+
 
 CalculatorApp().run()
